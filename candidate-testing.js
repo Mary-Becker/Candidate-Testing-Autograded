472f545887b22fe9128222a9cca3cb220a1ed76a
JC <marybecker053@gmail.com>
2024-04-03 23:56:37 -0500

    Part 2 Save

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,37 +11,41 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["1. Who was the first American woman in space?", "2. True or false: 5 kilometers == 5000 meters?", "3. (5 + 3)2 * 10 = ?","4. Given the array [8,'orbit','trajectory', 45], what entry is at index 2?", "5. What is the minimum crew size for the ISS? " ];
+let correctAnswers = ["Sally Ride", "True", "40","Trajectory","3"];
+let candidateAnswers = " ";
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName = input.question ("What is your name?")
-
+candidateName = input.question("What is your name?")
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question)
-
+  for (let i = 0; i < questions.length; i++) {
+  candidateAnswers = input.question(questions[i]);
+  return candidateAnswers
+  }
+  // candidateAnswers = (input.question(questions))
 } 
 
-function gradeQuiz(candidateAnswers) {
+function gradeQuiz(correctAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer === correctAnswer) {
-  console.log("Correct!")
+for (let i = 0; i < correctAnswers.length; i++) {
+  if (candidateAnswers === correctAnswers[i]) {
+     console.log (`Correct! The answer was ${correctAnswers[1]}`);
 } else {
-  console.log("Wrong answer.")
+  console.log (`Incorrect. The answer is ${correctAnswers[i]}, not ${candidateAnswers}.`);
 }
-
+// START BACK HER AFTER WORK! YOU NEED TO MAKE A LOOP TO PULL OUT THE INDIVIDUAL INDEXES OF THE CORRECT ANSWERS, MAYBE PUT A CONDITIONAL INTO A LOOP.
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
 
   return grade;
+}
 }
 
 function runProgram() {
